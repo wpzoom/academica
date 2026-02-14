efc15bf0db113738c6720f999085d609f5f9781a
Pavel Ciorici <cioricip@gmail.com>
2026-02-14 15:32:31 +0000

    fix search form #

--- searchform.php
+++ searchform.php
@@ -2,9 +2,10 @@
 /**
  * @package Academica
  */
+$unique_id = wp_unique_id( 'search-form-' );
 ?>
-<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
-	<label for="s" class="screen-reader-text"><?php _e( 'Search for:', 'academica' ); ?></label>
-	<input id="s" type="search" name="s" placeholder="<?php esc_attr_e( 'Search', 'academica' ); ?>" aria-label="<?php esc_attr_e( 'Search', 'academica' ); ?>">
-	<button id="searchsubmit" name="submit" type="submit" aria-label="<?php esc_attr_e( 'Submit search', 'academica' ); ?>"><?php _e( 'Search', 'academica' ); ?></button>
+<form method="get" id="<?php echo $unique_id; ?>" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
+	<label for="<?php echo $unique_id; ?>-input" class="screen-reader-text"><?php _e( 'Search for:', 'academica' ); ?></label>
+	<input id="<?php echo $unique_id; ?>-input" type="search" name="s" placeholder="<?php esc_attr_e( 'Search', 'academica' ); ?>" aria-label="<?php esc_attr_e( 'Search', 'academica' ); ?>">
+	<button id="<?php echo $unique_id; ?>-submit" name="submit" type="submit" aria-label="<?php esc_attr_e( 'Submit search', 'academica' ); ?>"><?php _e( 'Search', 'academica' ); ?></button>
 </form>
